ea5b1a01a651a8120e67fbea7f59d36bf8c87828
zhixiang.yuan <im.yuanzx@gmail.com>
2019-12-31 13:45:37 +0800

    update file

--- spring-boot-tests/spring-boot-web-tests/src/main/java/me/yuanzx/MVCApplication.java
+++ spring-boot-tests/spring-boot-web-tests/src/main/java/me/yuanzx/MVCApplication.java
@@ -1,11 +1,9 @@
 package me.yuanzx;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 
 
 /**
